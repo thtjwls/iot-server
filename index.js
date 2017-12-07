3f10dfac46c2572c02a1938da6181e7b5fbbd501
Lee Ji Hoon <thtjwls@gmail.com>
2017-12-08 00:10:56 +0900

    로그 모듈 작성

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 /* ============== Packet Interface ============== */
-class Packets {
-
-}
 
 /* ======================= Config 부분 ========================= */
 /* socket io import */
@@ -12,6 +9,11 @@ var io = require('socket.io')(http);
 /* TCP import */
 var net = require('net');
 var server = net.createServer();
+
+/* 로그 파일을 만들기 위한 모듈 */
+var fs = require('fs');
+var path = require('path');
+var readfiles = require('readfiles');
 
 var TCP_PORT = 3000;
 var IO_PORT = 5000;
@@ -40,17 +42,24 @@ io.on('connection', function (socket) {
 
 /* 클라이언트 -> 장비 커넥터 */
 io.sockets.on('connection', function (socket) {
+
+    /* 이벤트 처리 */
     socket.on('send-packet', function (data) {
         console.log('send-packet 이벤트 !! : ');
         console.log(data.Sync1);
         console.log(' END === send-packet 이벤트 !! === ');
-        console.log('io connectoin sockets : ' + clients.length);
+        // console.log('io connectoin sockets : ' + clients.length);
+        console.log('Client -> Device : ' + new Date());
+        writeFile('client', JSON.stringify(data));
+
         clients.forEach(function (client) {
             var stream_port = client.remotePort;
-            console.log('Client -> Device : ' + new Date());
-
             client.write('adsf');
         });
+    })
+
+    socket.on('read-log', function () {
+        readDirList();
     })
 });
 
@@ -69,6 +78,9 @@ server.on('connection', function (socket) {
 
     socket.on('data', function (data) {
         console.log('Device -> Client : ' + new Date());
+        writeFile('device', data);
+        readDirList();
+
         
         // 장비 -> 클라이언트
         io.sockets.emit('receive-packet', data.toString());
@@ -80,3 +92,40 @@ server.on('connection', function (socket) {
 io.on('send-packet', function () {
     console.log('recevive packet !');
 });
+
+function readDirList() {
+    fs.readdir(__dirname + '/log/device/', (err, files) => {
+        io.sockets.emit('read-log-res', files);
+    })
+}
+
+function getNowTime() {
+    var time = new Date();
+    var y = time.getFullYear();
+    var m = time.getMonth() + 1;
+    var d = time.getDate();
+    var h = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
+    var i = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+    var s = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
+
+    if ( m < 10 ) { m = '0' + m; }
+    if ( d < 10 ) { d = '0' + d; }
+
+    var nowTime = y + '-' + m + '-' + d + 'A' + h + '-' +  i + '-' + s;
+    return nowTime;
+}
+
+function writeFile(u, data) {
+
+    var filename = 'log/' + u + '/' + getNowTime() + '.txt';
+
+    fs.open(filename, 'w', function (err, fd) {
+        if ( err ) throw err;
+    });
+
+
+    fs.writeFile(filename, data, 'utf8', function (err) {
+        console.log(getNowTime() + '에 로그 기록!');
+        console.log(err);
+    })
+}
