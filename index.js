709d65175562b8c909450a81b22f03d1467d7df6
jihoon <thtjwls@gmail.com>
2018-01-09 14:35:41 +0900

    패킷저장 toString -> parseInt 로 변경

--- index.js
+++ index.js
@@ -216,9 +216,9 @@ function buffer_decode(data) {
         hub_data.hcu_id = od[6];
 
         var ele_packet = '0x' + od[7].toString(16) + od[8].toString(16) + od[9].toString(16) + od[10].toString(16);
-        hub_data.electric = ele_packet.toString(10);
+        hub_data.electric = parseInt(ele_packet, 16) / 1000;
         var wat_packet = '0x' + od[11].toString(16) + od[12].toString(16) + od[13].toString(16) + od[14].toString(16);
-        hub_data.water = wat_packet.toString(10);
+        hub_data.water = parseInt(wat_packet, 16) / 1000;
         hub_data.ext1 = `${od[15]}:${od[16]}:${od[17]}:${od[18]}`;
         hub_data.ext1 = `${od[19]}:${od[20]}:${od[21]}:${od[22]}`;
         hub_data.ext1 = `${od[23]}:${od[24]}:${od[25]}:${od[26]}`;
